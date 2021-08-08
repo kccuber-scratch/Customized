e843086b5d34759a34755aa7cb35dfd6efe8af1f
LankyBox01 <79767244+CodeGuy92@users.noreply.github.com>
2021-08-08 19:43:29 +0300

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -1,4 +1,4 @@
-setTimeout(function(){
+window.onload = function() {
 document.body.style.backgroundColor = "#171717";
 document.body.style.color = "white";
 document.getElementsByClassName("dropdown_dropdown_2cQyf")[0].style.borderColor = "#404040";
@@ -8,4 +8,4 @@ for(var i = 0; i < els.length; i++)
 {
   els[i].style.filter = "brightness(5)";
 }
-}, 700)
+};
