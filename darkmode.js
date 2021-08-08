db7f7a3d3a0cbbdf3edb3ef44dd3d6da6a48976c
LankyBox01 <79767244+CodeGuy92@users.noreply.github.com>
2021-08-08 19:37:35 +0300

    I'm done.

--- darkmode.js
+++ darkmode.js
@@ -1,13 +1,11 @@
-window.addEventListener('load', (event) => {
+setTimeout(function(){
 document.body.style.backgroundColor = "#171717";
 document.body.style.color = "white";
 document.getElementsByClassName("dropdown_dropdown_2cQyf")[0].style.borderColor = "#404040";
 
-setTimeout(function(){
 var els = document.getElementsByClassName("tool-select-base_tool-select-icon_2TT3t");
 for(var i = 0; i < els.length; i++)
 {
   els[i].style.filter = "brightness(5)";
 }
-}, 1000)
-});
+}, 2000)
