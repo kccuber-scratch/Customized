3ee804f2683f7903728a73ee7609e2492380ffb2
LankyBox01 <79767244+CodeGuy92@users.noreply.github.com>
2021-08-08 19:40:55 +0300

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -1,3 +1,7 @@
+document.body.style.backgroundColor = "#171717";
+document.body.style.color = "white";
+document.getElementsByClassName("dropdown_dropdown_2cQyf")[0].style.borderColor = "#404040";
+
 var els = document.getElementsByTagName("img");
 for(var i = 0; i < els.length; i++)
 {
