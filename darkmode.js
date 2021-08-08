ada69c4623ae88557ce352dc9816fdeb576b5cd5
LankyBox01 <79767244+CodeGuy92@users.noreply.github.com>
2021-08-08 19:39:24 +0300

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -1,11 +1,7 @@
 setTimeout(function(){
-document.body.style.backgroundColor = "#171717";
-document.body.style.color = "white";
-document.getElementsByClassName("dropdown_dropdown_2cQyf")[0].style.borderColor = "#404040";
-
-var els = document.getElementsByClassName("tool-select-base_tool-select-icon_2TT3t");
+var els = document.getElementsByTagName("img");
 for(var i = 0; i < els.length; i++)
 {
   els[i].style.filter = "brightness(5)";
 }
-}, 2000)
+}, 750)
