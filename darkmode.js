ec0faa80f1f921ac25f3e1e78db01caac6f82fa4
LankyBox01 <79767244+CodeGuy92@users.noreply.github.com>
2021-08-08 16:26:03 +0000

    theSE STUPID ERRORS

--- darkmode.js
+++ darkmode.js
@@ -1,4 +1,5 @@
 window.addEventListener('load', (event) => {
+setTimeout(function(){
 document.body.style.backgroundColor = "#171717";
 document.body.style.color = "white";
 document.getElementsByClassName("dropdown_dropdown_2cQyf")[0].style.borderColor = "#404040";
@@ -8,4 +9,5 @@ for(var i = 0; i < els.length; i++)
 {
   els[i].style.filter = "brightness(5)";
 }
+}, 1000)
 });
