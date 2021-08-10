be3bb860c7a1c6729a29b4edb830e5cbc5f0c748
LankyBox01 <79767244+CodeGuy92@users.noreply.github.com>
2021-08-10 18:17:56 +0000

    Add apply button

--- settings/save.js
+++ settings/save.js
@@ -11,11 +11,21 @@ if (document.getElementById("swatches").value == "true") {
   document.cookie = "swatches=false;path=/"
 }
 
-if (document.getElementById("expand").value == "true") {
-  document.cookie = "expand=true;path=/"
-}else{
-  document.cookie = "expand=false;path=/"
+window.location.href = "https://lankybox01.leahcimto.com/Customized/"
 }
 
-window.location.href = "https://lankybox01.leahcimto.com/Customized/"
+function save() {
+if (document.getElementById("dark").value == "true") {
+  document.cookie = "darkmode=true;path=/"
+}else{
+  document.cookie = "darkmode=false;path=/"
+}
+
+if (document.getElementById("swatches").value == "true") {
+  document.cookie = "swatches=true;path=/"
+}else{
+  document.cookie = "swatches=false;path=/"
+}
+
+alert("Changes applied!")
 }
