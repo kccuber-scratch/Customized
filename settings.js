b8dfa0c722ca21e8ea0fc10799f014de9795d656
LankyBox01 <79767244+CodeGuy92@users.noreply.github.com>
2021-08-10 17:05:08 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,3 +1,3 @@
 window.addEventListener('load', (event) => {
-document.getElementsByClassName("paint-editor_mod-mode-tools_1IXSj")[0].insertAdjacentHTML("beforeEnd", `<div style="border-left: 1px dashed #D9D9D9;margin-left: 15px; padding-left: 15px;"><button style="cursor:pointer;" onclick="window.location.href = '/settings/'">Settings</button></div>`);
+document.getElementsByClassName("paint-editor_mod-mode-tools_1IXSj")[0].insertAdjacentHTML("beforeEnd", `<div style="border-left: 1px dashed #D9D9D9;margin-left: 15px; padding-left: 15px;"><button style="cursor:pointer;" onclick="window.location.href = 'settings/'">Settings</button></div>`);
 });
