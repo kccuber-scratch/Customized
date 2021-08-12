582c260953555b18ed3684e3b02051af0ca86020
LankyBox01 <79767244+CodeGuy92@users.noreply.github.com>
2021-08-12 18:44:09 +0000

    Revert.exe

--- share.js
+++
@@ -1,3 +0,0 @@
-window.addEventListener('load', (event) => {
-	document.getElementsByClassName("paint-editor_mod-mode-tools_1IXSj")[0].insertAdjacentHTML("beforeEnd", `<div style="border-left: 1px dashed #D9D9D9;margin-left: 15px; padding-left: 15px;"><button style="cursor:pointer;" onclick="window.location.href = 'https://github.com/CodeGuy92/Customized/issues/15'">Share</button></div>`);
-});
