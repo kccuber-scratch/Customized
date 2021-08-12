314d52a5b97109f7bd98904a4996765274939c54
CST1229 <68464103+CST1229@users.noreply.github.com>
2021-08-12 10:31:42 +0200

    more comment

--- swatches.js
+++ swatches.js
@@ -174,7 +174,9 @@ if (document.cookie.includes("swatches=true") || new URL(window.location).search
 
 //Function for converting HSV to RGB, thanks stackoverflow (actually from gist.github.com/mjackson/5311256)
 //Slightly minified
-//Keeping this outside in case something else wants to use it, accepts HSV values from 0 to 1, not 0 to 100, so divide by 100
+//Keeping this outside in case something else wants to use it
+//Accepts HSV values from 0 to 1, not 0 to 100, so divide by 100
+//Returns decimal RGB values from 0 to 255
 function HSVtoRGB(h, s, v) {var r, g, b, i, f, p, q, t;
 	i = Math.floor(h * 6); f = h * 6 - i; p = v * (1 - s); q = v * (1 - f * s); t = v * (1 - (1 - f) * s);
 	switch (i % 6) { case 0: r = v, g = t, b = p; break; case 1: r = q, g = v, b = p; break; case 2: r = p, g = v, b = t; break; case 3: r = p, g = q, b = v; break; case 4: r = t, g = p, b = v; break; case 5: r = v, g = p, b = q; break;}
