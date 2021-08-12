5bf8c75d9662f78065d75bae025d7a4d89eb1f8c
LankyBox01 <79767244+CodeGuy92@users.noreply.github.com>
2021-08-12 17:35:10 +0300

    Update save.js

--- settings/save.js
+++ settings/save.js
@@ -17,13 +17,3 @@ function apply() {
 
 	alert("Changes applied!");
 }
-
-//Automatically set selected options
-function setOption(id, valueToSelect) {    
-    let element = document.getElementById(id);
-    element.value = valueToSelect;
-}
-if (document.cookie.includes("darkmode=true")) setOption("dark", "true");
-if (document.cookie.includes("swatches=true")) setOption("swatches", "true");
-if (document.cookie.includes("swatches=experimental")) setOption("swatches", "experimental");
-if (document.cookie.includes("icons=true")) setOption("icons", "true");
