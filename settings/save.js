3e2543f3a67e86044f1e2405349a26720411109f
CST1229 <68464103+CST1229@users.noreply.github.com>
2021-08-20 11:58:35 +0200

    Add custom font option, fix more fonts option

--- settings/save.js
+++ settings/save.js
@@ -8,6 +8,7 @@ function change() {
 	document.cookie = "bigger=" + document.getElementById("bigger").value + ";expires=Fri, 31 Dec 9999 23:59:59 GMT;path=/"
 
 	document.cookie = "fonts=" + document.getElementById("fonts").value + ";expires=Fri, 31 Dec 9999 23:59:59 GMT;path=/"
+	document.cookie = "customfonts=" + document.getElementById("customfonts").value + ";expires=Fri, 31 Dec 9999 23:59:59 GMT;path=/"
 }
 
 function save() {
